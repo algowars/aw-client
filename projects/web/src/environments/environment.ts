a1671f98ffdbe96f4036f3d8ab86da729eb52f06
admclamb <adylanmclamb@yahoo.com>
2026-02-05 00:10:03 -0500

    Fix typo

--- projects/web/src/environments/environment.ts
+++ projects/web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiServerUrl: import.meta.env['G_APP_API_SERVER_URL'] as string,
+  apiServerUrl: import.meta.env['NG_APP_API_SERVER_URL'] as string,
   auth: {
     domain: import.meta.env['NG_APP_AUTH_DOMAIN'] as string,
     clientId: import.meta.env['NG_APP_AUTH_CLIENT_ID'] as string,
