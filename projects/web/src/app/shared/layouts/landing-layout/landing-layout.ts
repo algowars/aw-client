3cab2fff0ef0815b5034195aa68f0cba4b90cf52
admclamb <adylanmclamb@yahoo.com>
2026-02-06 00:17:19 -0500

    Update pipeline

--- projects/web/src/app/shared/layouts/landing-layout/landing-layout.ts
+++ projects/web/src/app/shared/layouts/landing-layout/landing-layout.ts
@@ -4,6 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-landing-layout',
   imports: [],
   templateUrl: './landing-layout.html',
-  styleUrl: './landing-layout.css',
 })
 export class LandingLayout {}
