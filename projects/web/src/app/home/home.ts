68d0714aa1bfa3279bd3f699aadd9feb6c121adf
admclamb <adylanmclamb@yahoo.com>
2026-02-06 19:08:11 -0500

    Update variables

--- projects/web/src/app/home/home.ts
+++ projects/web/src/app/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  protected serverUrl: string = environment.apiServerUrl;
+}
---
+++ projects/web/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  apiServerUrl: import.meta.env['NG_APP_API_SERVER_URL'] as string,
+  auth: {
+    domain: import.meta.env['NG_APP_AUTH_DOMAIN'] as string,
+    clientId: import.meta.env['NG_APP_AUTH_CLIENT_ID'] as string,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+  },
+};
