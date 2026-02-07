f979c3fde9fbc93938a27708ce51d556066e3735
admclamb <adylanmclamb@yahoo.com>
2026-02-06 23:36:02 -0500

    Update landing

---
+++ projects/web/src/app/shared/navbars/landing-navbar/landing-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-landing-navbar',
+  imports: [RouterLink],
+  templateUrl: './landing-navbar.html',
+})
+export class LandingNavbar {
+
+}
