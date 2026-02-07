3de2d3e1284ee5fb4831eec6433c2c2f857d7e8e
admclamb <adylanmclamb@yahoo.com>
2026-02-06 19:28:30 -0500

    fix pathing

--- projects/web/src/app/app.ts
+++ projects/web/src/app/app.ts
@@ -2,11 +2,10 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { ThemeSwitcher } from './shared/theme/theme-switcher/theme-switcher';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, ButtonModule, ThemeSwitcher],
+  imports: [RouterOutlet, CommonModule, ButtonModule],
   templateUrl: './app.html',
 })
 export class App {}
